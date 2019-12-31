b86732e9f349208bb3a198bbbfb2a0ccb8b006c2
misha <blava1979@gmail.com>
2019-12-31 15:45:33 +0400

    added custom marker with custom icon and description

--- src/app/components/map-dahsboard/map-dahsboard.component.ts
+++ src/app/components/map-dahsboard/map-dahsboard.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit , ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 
+import {} from 'googlemaps';
+
 @Component({
   selector: 'app-map-dahsboard',
   templateUrl: './map-dahsboard.component.html',
   styleUrls: ['./map-dahsboard.component.css']
 })
 export class MapDahsboardComponent implements OnInit , AfterViewInit{
-  map: google.maps.Map;
   @ViewChild('mapWrapper', {static: false}) mapElement: ElementRef;
 
   constructor() { }
@@ -23,6 +24,8 @@ export class MapDahsboardComponent implements OnInit , AfterViewInit{
   initGMap() {
     const lngLat = new google.maps.LatLng(
       41.692417, 44.803771);
+      const contentString = `<h3>სათაური</h3>უბრალო ტექსტი`;
+
     const mapOptions: google.maps.MapOptions = {
       center: lngLat,
       zoom: 16,
@@ -31,7 +34,23 @@ export class MapDahsboardComponent implements OnInit , AfterViewInit{
       streetViewControl: false,
       zoomControl : false
     };
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);    
+    const map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);    
+    const markerPos = new google.maps.LatLng(41.692232, 44.804472);
+    const marker = new google.maps.Marker({
+      position: markerPos,
+      map: map,
+      title: 'Hello World!',
+      icon : 'https://maps.google.com/mapfiles/kml/shapes/parking_lot_maps.png'
+  });
+
+  const infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
   }
 
 }
